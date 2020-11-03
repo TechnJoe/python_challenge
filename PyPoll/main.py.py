02b2f7fc20c99cbbc8b657eaffa0b5ae640039e1
Moja One <joalex09@gmail.com>
2020-11-03 09:57:15 -0500

    debugged the paypoll code and now the output is succesful

--- PyPoll/main.py.py
+++ PyPoll/main.py.py
@@ -3,49 +3,37 @@ import csv
 
 csvpath = os.path.join("Resources","election_data.csv")
 
+
+Correy_percent = 0
+Khan_percent = 0
+Li_percent = 0
+Otooley_percent = 0
+Correy_votes = 1
+Otooley_votes = 1
+Khan_votes = 1
+Li_votes = 1        
+Total_votes_cast = 0
+winner = ""
+row_count = 0
+
 with open (csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
-    print(csvreader)
-
+    #print(csvreader)
+    
     csv_header = next(csvreader)
 
     #print(f"csv Header: {csv_header}")
-    row_count  = sum(1 for row in csvreader)
-    print (row_count)
-    Total_votes_cast = 0
-
-    Correy_percent = 0
-    Khan_percent = 0
-    Li_percent = 0
-    Otooley_percent = 0
-
-    Correy_votes = 1
-    Otooley_votes = 1
-    Khan_votes = 1
-    Li_votes = 1        
-    Khan = 0
-    Correy = 0
-    Li = 0
-    Otooley = 0
-   
-
+    #row_count  = sum(1 for row in csvreader)
+    #print (row_count)
+    
     for row in csvreader:
         #print(row)
-        
-        #declare all variables used in code
-        #candidates  listed are Khan, Correy, Li, and O'Tooley
-        candidate =""
-        candidate_list = []
-        Total_votes_cast = 0
-        candidate_votes = 0
-        percentage_list = []
-        winner = ""
-        percent_vote =[]
+        row_count  = row_count + 1
 
         #Total number of votes won by each candidate
 
-        Total_votes_cast = row_count
+        #Total_votes_cast = row_count
         
         if (row[2] == "Correy"):
             Correy_votes += 1
@@ -53,12 +41,12 @@ with open (csvpath) as csvfile:
             Li_votes += 1
         elif (row[2] == "O'Tooley"):
             Otooley_votes += 1
-        else: (row[2] == "Khan")
-        Khan_votes += 1
+        elif (row[2] == "Khan"):
+            Khan_votes += 1
         
             
-        #Total votes cast 
-    Total_votes=row_count
+    #Total votes cast 
+    Total_votes_cast = row_count
         
        #Percentage of votes won by each candidate
     try:
@@ -85,8 +73,9 @@ with open (csvpath) as csvfile:
     
 
     #The winner of the election based on popular vote.
-        
-    winner = max("Correy_votes","Li_votes","Otooley_votes","Khan_vote")
+
+    winner = max(Correy_votes,Li_votes,Otooley_votes,Khan_votes)   
+    #winner = max("Correy_votes","Li_votes","Otooley_votes","Khan_vote")
             
     if winner == Correy_votes:
         winner_name = "Correy"
@@ -94,22 +83,20 @@ with open (csvpath) as csvfile:
         winner_name = "Li"
     elif winner == Otooley_votes:
         winner_name = "O'Tooley"
-    else:winner == Khan_votes 
-    winner_name = "Khan"
+    elif winner == Khan_votes:
+        winner_name = "Khan"
     
-        
-
     #code to create path foroutput file
 
     output = ""
     output +=(F"\n\n")
     output +=(f"Election_Results\n")
-    output +=("Total votes cast: {Total_votes_cast}\n")
-    output +=(f"Correy: {Correy_percent}% ({Correy})\n")
-    output +=(f"Li: {Li_percent}% ({Li})\n")
-    output +=(f"O'Tooley: {Otooley_percent}% ({Otooley})\n")
-    output +=(f"Khan: {Khan_percent}% ({Khan})\n")
-    output +=(f"Winner: {winner}\n")
+    output +=(f"Total votes cast: { Total_votes_cast }\n")
+    output +=(f"Correy: {Correy_percent}% ({Correy_votes})\n")
+    output +=(f"Li: {Li_percent}% ({Li_votes})\n")
+    output +=(f"O'Tooley: {Otooley_percent}% ({Otooley_votes})\n")
+    output +=(f"Khan: {Khan_percent}% ({Khan_votes})\n")
+    output +=(f"Winner: {winner_name} - {winner}\n")
 
     #code to print analysis
     print(output)
